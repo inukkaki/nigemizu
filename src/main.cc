234ee5e7aa3e2f193354624a385be111a085a265
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-08-12 18:26:02 +0900

    Include SDL2 libraries

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 
+#include "SDL2/SDL.h"
+
 int main(int argc, char* argv[]) {
-    std::cout << "hello world" << std::endl;
+    std::cout << "include SDL2 libraries" << std::endl;
     return 0;
 }
