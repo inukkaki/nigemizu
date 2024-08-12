9b87e1475dbe221c66c8eea94cbc987adee975a5
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-08-12 20:33:13 +0900

    Create CreateWindow()

--- src/interfaces/boot.cc
+++ src/interfaces/boot.cc
@@ -3,6 +3,7 @@
 #include "SDL2/SDL.h"
 
 #include "interfaces/modal.h"
+#include "models/config.h"
 
 namespace nigemizu::interfaces::boot {
 
@@ -20,14 +21,31 @@ bool InitSdl(Uint32 flags) {
     return succeeds;
 }
 
+bool CreateWindow(SDL_Window*& window) {
+    bool succeeds = true;
+    namespace config = nigemizu::models::config;
+    window = SDL_CreateWindow(
+        config::kTitle, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        config::kWindowWidth, config::kWindowHeight, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        succeeds = false;
+        modal::ShowErrorMessage(
+            "Initialization Error",
+            "Could not create a main window.",
+            SDL_GetError());
+    }
+    return succeeds;
+}
+
 }  // namespace
 
 bool InitGui(SDL_Window*& window, SDL_Renderer*& renderer) {
     // TODO: Implement this!
     bool succeeds = false;
     if (InitSdl(SDL_INIT_VIDEO)) {
-        // ...
-        succeeds = true;
+        if (CreateWindow(window) /* && ... */) {
+            succeeds = true;
+        }
     }
     return succeeds;
 }
