14c410fbc5815d4932fbe6976d74fa5c1d0ce72d
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-08-12 21:14:22 +0900

    Create interfaces/framerate

---
+++ src/interfaces/framerate.cc
@@ -0,0 +1,7 @@
+#include "interfaces/framerate.h"
+
+namespace nigemizu::interfaces::framerate {
+
+// ...
+
+}  // nigemizu::interfaces::framerate
---
+++ src/interfaces/framerate.h
@@ -0,0 +1,10 @@
+#ifndef NIGEMIZU_INTERFACES_FRAMERATE_H_
+#define NIGEMIZU_INTERFACES_FRAMERATE_H_
+
+namespace nigemizu::interfaces::framerate {
+
+// ...
+
+}  // nigemizu::interfaces::framerate
+
+#endif  // NIGEMIZU_INTERFACES_FRAMERATE_H_
