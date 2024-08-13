d166f084470d32a610d20d0580c62a644e2ebe56
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-08-14 00:28:46 +0900

    Create SingletonFinalizer class

--- src/models/singleton.cc
+++ src/models/singleton.cc
@@ -1,6 +1,31 @@
 #include "models/singleton.h"
 
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <vector>
+
 namespace nigemizu::models::singleton {
+
+namespace impl {
+
+std::vector<std::function<void()>> SingletonFinalizer::finalizers_;
+
+void SingletonFinalizer::AddFinalizer(std::function<void()>&& func) {
+    finalizers_.emplace_back(std::forward<decltype(func)>(func));
+}
+
+void SingletonFinalizer::Finalize() {
+    std::for_each(
+        finalizers_.crbegin(),
+        finalizers_.crend(),
+        [](auto&& finalizer) {
+            finalizer();
+        }
+    ), finalizers_.clear();
+}
+
+}  // namespace impl
 
 // ...
 
--- src/models/singleton.h
+++ src/models/singleton.h
@@ -1,7 +1,25 @@
 #ifndef NIGEMIZU_MODELS_SINGLETON_H_
 #define NIGEMIZU_MODELS_SINGLETON_H_
 
+#include <functional>
+#include <vector>
+
 namespace nigemizu::models::singleton {
+
+namespace impl {
+
+class SingletonFinalizer final {
+public:
+    SingletonFinalizer() = delete;
+
+    static void AddFinalizer(std::function<void()>&& func);
+    static void Finalize();
+
+private:
+    static std::vector<std::function<void()>> finalizers_;
+};
+
+}  // namespace impl
 
 // ...
 
