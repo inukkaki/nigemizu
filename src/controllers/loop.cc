9a26c6f7f94bea0114403b848a6ea953d0790a5a
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-08-13 21:14:57 +0900

    Create MainLoop()

--- src/controllers/loop.cc
+++ src/controllers/loop.cc
@@ -1,7 +1,43 @@
 #include "controllers/loop.h"
 
+#include "SDL2/SDL.h"
+
+// DEBUG
+#include <iostream>
+#include "interfaces/framerate.h"
+
 namespace nigemizu::controllers::loop {
 
-// ...
+void MainLoop(SDL_Window* window, SDL_Renderer* renderer) {
+    // DEBUG
+    bool running = true;
+
+    using nigemizu::interfaces::framerate::FrameRateBalancer;
+    using nigemizu::interfaces::framerate::FrameRateMeasurer;
+    FrameRateBalancer frb(60);
+    FrameRateMeasurer frm;
+
+    using nigemizu::models::timer::SimpleTimer;
+    SimpleTimer timer;
+
+    double measured_frame_rate = 0.0;
+
+    frb.SetTimer();
+    frm.SetTimer();
+
+    timer.Set();
+
+    while (running) {
+        if (timer.GetElapsedTime() > 5000) {
+            running = false;
+        }
+
+        if (frm.MeasureFrameRate(measured_frame_rate)) {
+            std::cout << measured_frame_rate << " fps" << std::endl;
+        }
+
+        frb.Balance();
+    }
+}
 
 }  // namespace nigemizu::controllers::loop
--- src/controllers/loop.h
+++ src/controllers/loop.h
@@ -1,9 +1,11 @@
 #ifndef NIGEMIZU_CONTROLLERS_LOOP_H_
 #define NIGEMIZU_CONTROLLERS_LOOP_H_
 
+#include "SDL2/SDL.h"
+
 namespace nigemizu::controllers::loop {
 
-// ...
+void MainLoop(SDL_Window* window, SDL_Renderer* renderer);
 
 }  // namespace nigemizu::controllers::loop
 
