35f08a1ab1cf12f73490cab8460b4a4a27443d31
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-08-22 23:12:38 +0900

    Create Keyboard class

--- src/models/keyboard.h
+++ src/models/keyboard.h
@@ -1,12 +1,37 @@
 #ifndef NIGEMIZU_MODELS_KEYBOARD_H_
 #define NIGEMIZU_MODELS_KEYBOARD_H_
 
+#include <array>
+
 namespace nigemizu::models::keyboard {
 
 enum class KeyCode : unsigned char {
     kA,
     kD,
     kMax,
+};
+
+inline constexpr int kNumOfKeyCodes = static_cast<int>(KeyCode::kMax);
+
+namespace impl {
+
+using KeyArray = std::array<bool, kNumOfKeyCodes>;
+
+}  // namespace impl
+
+class Keyboard {
+public:
+    Keyboard() {
+        pressed_.fill(false);
+        pressed_prev_.fill(false);
+    }
+    ~Keyboard() {}
+
+    // ...
+
+private:
+    impl::KeyArray pressed_;
+    impl::KeyArray pressed_prev_;
 };
 
 }  // namespace nigemizu::models::keyboard
