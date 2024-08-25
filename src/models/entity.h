ee1a1c40c57eea08c70b0b6a243bde2e1b114636
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-08-25 19:09:06 +0900

    Create Data struct

--- src/models/entity.h
+++ src/models/entity.h
@@ -1,9 +1,21 @@
 #ifndef NIGEMIZU_MODELS_ENTITY_H_
 #define NIGEMIZU_MODELS_ENTITY_H_
 
+#include "models/math.h"
+
 namespace nigemizu::models::entity {
 
-// ...
+namespace impl {
+
+namespace math = nigemizu::models::math;
+
+}  // namespace impl
+
+struct Data {
+    impl::math::Vector2D r_;  // px
+    impl::math::Vector2D v_;  // px frame-1
+    impl::math::Vector2D a_;  // px frame-2
+};
 
 }  // namespace nigemizu::models::entity
 
