6b335994bbfa9da5cb90c69c6e2f68c55bcfe460
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-09-28 13:57:26 +0900

    Create models/pool

---
+++ src/models/pool.h
@@ -0,0 +1,10 @@
+#ifndef NIGEMIZU_MODELS_POOL_H_
+#define NIGEMIZU_MODELS_POOL_H_
+
+namespace nigemizu::models::pool {
+
+// ...
+
+}  // namespace nigemizu::models::pool
+
+#endif  // NIGEMIZU_MODELS_POOL_H_
