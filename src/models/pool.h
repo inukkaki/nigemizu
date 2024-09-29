ed9117cfb09c2b2b9a9ce5742aa5d26d0315a09b
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-09-29 23:25:30 +0900

    Remove Object concept and ObjectPool

--- src/models/pool.h
+++ src/models/pool.h
@@ -1,34 +1,9 @@
 #ifndef NIGEMIZU_MODELS_POOL_H_
 #define NIGEMIZU_MODELS_POOL_H_
 
-#include <cassert>
-
 namespace nigemizu::models::pool {
 
-namespace impl {
-
-template <typename T>
-concept Object = requires (T& x) {
-    T();
-};
-
-}  // namespace impl
-
-template <impl::Object T>
-class ObjectPool {
-public:
-    explicit ObjectPool(int size) : size_(size) {
-        assert(size_ > 0);
-        members_ = new T[size_];
-    }
-    virtual ~ObjectPool() {
-        delete[] members_;
-    }
-
-protected:
-    int size_;
-    T* members_;
-};
+// ...
 
 }  // namespace nigemizu::models::pool
 
--- tests/pool.cc
+++ tests/pool.cc
@@ -4,26 +4,8 @@
 
 #include "models/pool.h"
 
-class A {
-public:
-    A() { std::cout << ++count_ << std::endl; }
-
-private:
-    inline static int count_ = 0;
-};
-
-class B {
-public:
-    B() = delete;
-};
-
 int main() {
-    using nigemizu::models::pool::ObjectPool;
-
-    ObjectPool<A> pool1(5);
-    //ObjectPool<A> pool2(-5);  // Cause assertion
-
-    //ObjectPool<B> pool3(5);  // Cause template constraint failure
+    std::cout << "no impl here yet" << std::endl;
 
     return 0;
 }
