353c2afc18a171b9e9461ad0a57fa564b5dccb96
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-09-29 23:45:21 +0900

    Modify the type of SimpleTimer::start_time_

--- src/models/timer.cc
+++ src/models/timer.cc
@@ -1,15 +1,18 @@
 #include "models/timer.h"
 
+#include <cstdint>
+
 #include "SDL2/SDL.h"
 
 namespace nigemizu::models::timer {
 
 void SimpleTimer::Set() {
-    start_time_ = SDL_GetTicks64();
+    start_time_ = static_cast<std::uint64_t>(SDL_GetTicks64());
 }
 
-unsigned long long int SimpleTimer::GetElapsedTime() const {
-    Uint64 elapsed_time = SDL_GetTicks64() - start_time_;
+std::uint64_t SimpleTimer::GetElapsedTime() const {
+    std::uint64_t elapsed_time
+        = static_cast<std::uint64_t>(SDL_GetTicks64()) - start_time_;
     return elapsed_time;
 }
 
