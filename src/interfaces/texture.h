c16ab8b0752ffef18ec6f29b2b8a27a915e975d3
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-10-08 12:57:52 +0900

    Create Texture class

--- src/interfaces/texture.h
+++ src/interfaces/texture.h
@@ -1,9 +1,19 @@
 #ifndef NIGEMIZU_INTERFACES_TEXTURE_H_
 #define NIGEMIZU_INTERFACES_TEXTURE_H_
 
+#include <memory>
+
+#include "SDL2/SDL.h"
+
 namespace nigemizu::interfaces::texture {
 
-// ...
+class Texture {
+public:
+    Texture() : texture_(nullptr) {}
+
+private:
+    SDL_Texture* texture_;
+};
 
 }  // namespace nigemizu::interfaces::texture
 
