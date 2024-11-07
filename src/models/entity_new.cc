b782a5b6eb7ded8d2cf06b672c59fa0eee5c6c21
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-11-07 16:39:38 +0900

    Create models/entity_new

---
+++ src/models/entity_new.cc
@@ -0,0 +1,8 @@
+#include "models/entity_new.h"
+
+namespace nigemizu::models::entity_new {
+    // TODO: Replace 'entity_new' with 'entity' later.
+
+// ...
+
+}  // namespace nigemizu::models::entity_new
---
+++ src/models/entity_new.h
@@ -0,0 +1,12 @@
+#ifndef NIGEMIZU_MODELS_ENTITY_NEW_H_
+    // TODO: Replace 'ENTITY_NEW' in this macro with 'ENTITY' later.
+#define NIGEMIZU_MODELS_ENTITY_NEW_H_
+
+namespace nigemizu::models::entity_new {
+    // TODO: Replace 'entity_new' with 'entity' later.
+
+// ...
+
+}  // namespace nigemizu::models::entity_new
+
+#endif  // NIGEMIZU_MODELS_ENTITY_NEW_H_
