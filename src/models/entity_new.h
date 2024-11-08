760c814c5d0675924dda8ed697b80920ea534b00
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-11-08 14:07:45 +0900

    Create Positional struct

--- src/models/entity_new.h
+++ src/models/entity_new.h
@@ -2,15 +2,31 @@
     // TODO: Replace 'ENTITY_NEW' in this macro with 'ENTITY' later.
 #define NIGEMIZU_MODELS_ENTITY_NEW_H_
 
+#include "models/math.h"
+
 namespace nigemizu::models::entity_new {
     // TODO: Replace 'entity_new' with 'entity' later.
+
+namespace impl {
+
+namespace math = nigemizu::models::math;
+
+}  // namespace impl
+
+struct Positional {
+    impl::math::Vector2D r;  // px
+    impl::math::Vector2D v;  // px s-1
+    impl::math::Vector2D a;  // px s-2
+
+    impl::math::Vector2D f;  // Sum of external forces; kg px s-2
+};
 
 class Entity {
 public:
     Entity() {}
 
 private:
-    // ...
+    Positional p_;
 };
 
 }  // namespace nigemizu::models::entity_new
