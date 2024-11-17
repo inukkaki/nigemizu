562ddeeb01330f3cf75963ddbc40dcfca4581d06
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-11-17 21:20:37 +0900

    Create entity/entity

---
+++ src/entity/entity.cc
@@ -0,0 +1,7 @@
+#include "entity/entity.h"
+
+namespace nigemizu::entity::entity {
+
+// ...
+
+}  // namespace nigemizu::entity::entity
---
+++ src/entity/entity.h
@@ -0,0 +1,10 @@
+#ifndef NIGEMIZU_ENTITY_ENTITY_H_
+#define NIGEMIZU_ENTITY_ENTITY_H_
+
+namespace nigemizu::entity::entity {
+
+// ...
+
+}  // namespace nigemizu::entity::entity
+
+#endif  // NIGEMIZU_ENTITY_ENTITY_H_
