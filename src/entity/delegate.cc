b80748e0c00aa410588224f001231cfd5be3219d
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-11-17 19:55:21 +0900

    Create entity/delegate

---
+++ src/entity/delegate.cc
@@ -0,0 +1,7 @@
+#include "entity/delegate.h"
+
+namespace nigemizu::entity::delegate {
+
+// ...
+
+}  // namespace nigemizu::entity::delegate
---
+++ src/entity/delegate.h
@@ -0,0 +1,10 @@
+#ifndef NIGEMIZU_ENTITY_DELEGATE_H_
+#define NIGEMIZU_ENTITY_DELEGATE_H_
+
+namespace nigemizu::entity::delegate {
+
+// ...
+
+}  // namespace nigemizu::entity::delegate
+
+#endif  // NIGEMIZU_ENTITY_DELEGATE_H_
