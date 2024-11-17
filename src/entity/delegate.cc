6d9a319a44f07eb53ce3362ee5eaf771e74e0840
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-11-17 21:13:25 +0900

    Create UpdateRDelegate class and its subclasses

--- src/entity/delegate.cc
+++ src/entity/delegate.cc
@@ -1,7 +1,18 @@
 #include "entity/delegate.h"
 
+#include "entity/base.h"
+
 namespace nigemizu::entity::delegate {
 
-// ...
+namespace impl {
+
+namespace base = nigemizu::entity::base;
+
+}  // namespace impl
+
+void AddVToR::UpdateR(impl::base::BaseEntity& self, float dt) const {
+    impl::base::Positional& pos = self.pos();
+    pos.r += pos.v*dt;
+}
 
 }  // namespace nigemizu::entity::delegate
--- src/entity/delegate.h
+++ src/entity/delegate.h
@@ -1,9 +1,39 @@
 #ifndef NIGEMIZU_ENTITY_DELEGATE_H_
 #define NIGEMIZU_ENTITY_DELEGATE_H_
 
+#include "entity/base.h"
+
 namespace nigemizu::entity::delegate {
 
-// ...
+namespace impl {
+
+namespace base = nigemizu::entity::base;
+
+}  // namespace impl
+
+class UpdateRDelegate {
+public:
+    UpdateRDelegate() {}
+    virtual ~UpdateRDelegate() = default;
+
+    virtual void UpdateR(impl::base::BaseEntity& self, float dt) const = 0;
+};
+
+class NotUpdateR final : public UpdateRDelegate {
+public:
+    NotUpdateR() {}
+
+    void UpdateR(impl::base::BaseEntity& self, float dt) const override {
+        /* NO-OP */
+    }
+};
+
+class AddVToR final : public UpdateRDelegate {
+public:
+    AddVToR() {}
+
+    void UpdateR(impl::base::BaseEntity& self, float dt) const override;
+};
 
 }  // namespace nigemizu::entity::delegate
 
