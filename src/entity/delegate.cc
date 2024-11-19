841bc499497cc67618ca5bf5930dc1642f15408d
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-11-19 13:51:54 +0900

    Remove UpdateADelegate and 8 other classes

--- src/entity/delegate.cc
+++ src/entity/delegate.cc
@@ -1,30 +1,7 @@
 #include "entity/delegate.h"
 
-#include "entity/base.h"
-
 namespace nigemizu::entity::delegate {
 
-namespace impl {
-
-namespace base = nigemizu::entity::base;
-
-}  // namespace impl
-
-void ApplyFToA::UpdateA(impl::base::BaseEntity& self) const {
-    impl::base::Positional& pos = self.pos();
-    const impl::base::PhysicalProperty& phys = self.phys();
-    pos.a = pos.f/phys.mass;
-    pos.f.Zero();
-}
-
-void AddAToV::UpdateV(impl::base::BaseEntity& self, float dt) const {
-    impl::base::Positional& pos = self.pos();
-    pos.v += pos.a*dt;
-}
-
-void AddVToR::UpdateR(impl::base::BaseEntity& self, float dt) const {
-    impl::base::Positional& pos = self.pos();
-    pos.r += pos.v*dt;
-}
+// ...
 
 }  // namespace nigemizu::entity::delegate
--- src/entity/delegate.h
+++ src/entity/delegate.h
@@ -1,87 +1,9 @@
 #ifndef NIGEMIZU_ENTITY_DELEGATE_H_
 #define NIGEMIZU_ENTITY_DELEGATE_H_
 
-#include "entity/base.h"
-
 namespace nigemizu::entity::delegate {
 
-namespace impl {
-
-namespace base = nigemizu::entity::base;
-
-}  // namespace impl
-
-class UpdateADelegate {
-public:
-    UpdateADelegate() {}
-    virtual ~UpdateADelegate() = default;
-
-    virtual void UpdateA(impl::base::BaseEntity& self) const = 0;
-};
-
-class NotUpdateA final : public UpdateADelegate {
-public:
-    NotUpdateA() {}
-
-    void UpdateA(impl::base::BaseEntity& self) const override {
-        /* NO-OP */
-    }
-};
-
-class ApplyFToA final : public UpdateADelegate {
-public:
-    ApplyFToA() {}
-
-    void UpdateA(impl::base::BaseEntity& self) const override;
-};
-
-class UpdateVDelegate {
-public:
-    UpdateVDelegate() {}
-    virtual ~UpdateVDelegate() = default;
-
-    virtual void UpdateV(impl::base::BaseEntity& self, float dt) const = 0;
-};
-
-class NotUpdateV final : public UpdateVDelegate {
-public:
-    NotUpdateV() {}
-
-    void UpdateV(impl::base::BaseEntity& self, float dt) const override {
-        /* NO-OP */
-    }
-};
-
-class AddAToV final : public UpdateVDelegate {
-public:
-    AddAToV() {}
-
-    void UpdateV(impl::base::BaseEntity& self, float dt) const override;
-};
-
-class UpdateRDelegate {
-public:
-    UpdateRDelegate() {}
-    virtual ~UpdateRDelegate() = default;
-
-    virtual void UpdateR(impl::base::BaseEntity& self, float dt) const = 0;
-};
-
-class NotUpdateR final : public UpdateRDelegate {
-public:
-    NotUpdateR() {}
-
-    void UpdateR(impl::base::BaseEntity& self, float dt) const override {
-        /* NO-OP */
-    }
-};
-
-class AddVToR final : public UpdateRDelegate {
-public:
-    AddVToR() {}
-
-    void UpdateR(impl::base::BaseEntity& self, float dt) const override;
-};
+// ...
 
 }  // namespace nigemizu::entity::delegate
 
