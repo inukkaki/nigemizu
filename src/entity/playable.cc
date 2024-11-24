5a762a5bea6ed5b678723f6dc7866215f3950001
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-11-24 19:36:33 +0900

    Create entity/playable

---
+++ src/entity/playable.cc
@@ -0,0 +1,7 @@
+#include "entity/playable.h"
+
+namespace nigemizu::entity::playable {
+
+// ...
+
+}  // namespace nigemizu::entity::playable
---
+++ src/entity/playable.h
@@ -0,0 +1,10 @@
+#ifndef NIGEMIZU_ENTITY_PLAYABLE_H_
+#define NIGEMIZU_ENTITY_PLAYABLE_H_
+
+namespace nigemizu::entity::playable {
+
+// ...
+
+}  // namespace nigemizu::entity::playable
+
+#endif  // NIGEMIZU_ENTITY_PLAYABLE_H_
