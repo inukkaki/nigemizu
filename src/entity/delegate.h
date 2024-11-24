360150367847cd984a135278cee0f5382f088e6f
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-11-24 21:21:21 +0900

    Create MoveDelegate class

--- src/entity/delegate.h
+++ src/entity/delegate.h
@@ -1,9 +1,23 @@
 #ifndef NIGEMIZU_ENTITY_DELEGATE_H_
 #define NIGEMIZU_ENTITY_DELEGATE_H_
 
+#include "entity/base.h"
+
 namespace nigemizu::entity::delegate {
 
-// ...
+namespace impl {
+
+namespace base = nigemizu::entity::base;
+
+}  // namespace impl
+
+class MoveDelegate {
+public:
+    MoveDelegate() = default;
+    virtual ~MoveDelegate() = default;
+
+    virtual void Move(impl::base::BaseEntity& self) = 0;
+};
 
 }  // namespace nigemizu::entity::delegate
 
