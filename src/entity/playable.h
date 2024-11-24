28a120fff961571d37207bbacab2069b14db9c5a
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-11-24 19:51:56 +0900

    Create KeyConfig struct

--- src/entity/playable.h
+++ src/entity/playable.h
@@ -2,14 +2,23 @@
 #define NIGEMIZU_ENTITY_PLAYABLE_H_
 
 #include "entity/entity.h"
+#include "interfaces/keyboard.h"
 
 namespace nigemizu::entity::playable {
 
 namespace impl {
 
 namespace eent = nigemizu::entity::entity;
+namespace kbd = nigemizu::interfaces::keyboard;
 
 }  // namespace impl
+
+struct KeyConfig {
+    impl::kbd::KeyCode transfer_up;
+    impl::kbd::KeyCode transfer_left;
+    impl::kbd::KeyCode transfer_right;
+    impl::kbd::KeyCode transfer_down;
+};
 
 class Playable : public impl::eent::Entity {
 public:
