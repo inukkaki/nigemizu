e3179a253740c02e4e45a43895da8e99b8dff3ab
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-11-24 21:53:15 +0900

    Remove UpdatePosition() from Entity

--- src/entity/entity.cc
+++ src/entity/entity.cc
@@ -1,23 +1,8 @@
 #include "entity/entity.h"
 
 #include "meta/assert.h"
-#include "models/config.h"
 
 namespace nigemizu::entity::entity {
-
-namespace impl {
-
-namespace config = nigemizu::models::config;
-
-}  // namespace impl
-
-void Entity::UpdatePosition() {
-    // NOTE: This function might contain more processes in the future.
-    float dt = impl::config::GetFrameDuration();
-    UpdateA();
-    UpdateV(dt);
-    UpdateR(dt);
-}
 
 void Entity::Move() {
     NIGEMIZU_ASSERT(move_);
--- src/entity/entity.h
+++ src/entity/entity.h
@@ -22,8 +22,6 @@ public:
         : move_(std::move(move)) {}
     virtual ~Entity() = default;
 
-    void UpdatePosition();
-
     void Move();
 
 private:
