2f7fcc0091a516717e1734f8936de8d8ac59f7b8
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-11-30 01:04:45 +0900

    Create core/utils

---
+++ src/core/utils.h
@@ -0,0 +1,10 @@
+#ifndef NIGEMIZU_CORE_UTILS_H_
+#define NIGEMIZU_CORE_UTILS_H_
+
+namespace nigemizu::core::utils {
+
+// ...
+
+}  // namespace nigemizu::core::utils
+
+#endif  // NIGEMIZU_CORE_UTILS_H_
