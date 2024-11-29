0b69d2898300098239819bdd1f4fb501c426668b
inukkaki <kgn.gkrk.rstb@gmail.com>
2024-11-30 00:17:44 +0900

    Create graphics/render

---
+++ src/graphics/render.cc
@@ -0,0 +1,7 @@
+#include "graphics/render.h"
+
+namespace nigemizu::graphics::render {
+
+// ...
+
+}  // namespace nigemizu::graphics::render
---
+++ src/graphics/render.h
@@ -0,0 +1,10 @@
+#ifndef NIGEMIZU_GRAPHICS_RENDER_H_
+#define NIGEMIZU_GRAPHICS_RENDER_H_
+
+namespace nigemizu::graphics::render {
+
+// ...
+
+}  // namespace nigemizu::graphics::render
+
+#endif  // NIGEMIZU_GRAPHICS_RENDER_H_
